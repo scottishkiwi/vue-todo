05020b2e5eb72ea81a0ecd02b3a4292cac3ec2b7
Daniel Medyckyj-Scott <dmedyckyj-scott@squiz.co.nz>
2019-05-08 14:59:38 +1200

    adding to Github

---
+++ src/components/Todos.vue
@@ -0,0 +1,27 @@
+<template>
+    <div id="Todos">
+        <div v-bind:key="todo.id" v-for="todo in todos" >
+            <TodoItem v-bind:todo="todo" v-on:del-todo="$emit('del-todo',todo.id)"/>
+        </div>
+    </div>
+</template>
+
+
+
+<script>
+import TodoItem from './TodoItem';
+
+export default {
+    name: "Todos",
+    components: {
+        TodoItem
+    },
+    props: ["todos"]
+}
+</script>
+
+<style>
+    #Todos{
+        margin: 20px; 
+    }
+</style>
---
+++ src/views/About.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="about">
+    <h1>About</h1>
+    <p>This is the TodoList app. Daniel is learning Vue.</p>
+  </div>
+</template>
+<style scoped>
+.about{
+  margin: 20px;
+}
+</style>
+
